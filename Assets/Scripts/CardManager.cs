76377dbeb30878dc0ed9e92d4f786aab25942215
aks024680 <130197640+aks024680@users.noreply.github.com>
2023-10-03 13:14:42 +0800

    Initial commit

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using UnityEngine;
+using Vuforia;
+
+namespace AR
+{
+    /// <summary>
+    /// 按鈕事件
+    /// </summary>
+    public class CardManager : MonoBehaviour
+    {
+        [SerializeField, Header("動畫控制器")]
+        private Animator ani;
+        [SerializeField, Header("虛擬按鈕攻擊")]
+        private VirtualButtonBehaviour vbbAttack;
+
+        private string parAttack = "AttackTrigger";
+
+        private void Awake()
+        {
+            vbbAttack.RegisterOnButtonPressed(PressAttackButton);
+        }
+
+        private void PressAttackButton(VirtualButtonBehaviour behaviour)
+        {
+            if (behaviour.VirtualButtonName == "Attack")
+            {
+                ani.SetTrigger(parAttack);
+            }
+        }
+    }
+}
+
